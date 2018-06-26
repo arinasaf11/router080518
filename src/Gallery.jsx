24c90d27705c55cf1f70319f98113a2afedb3d90
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2018-06-26 04:24:15 +0300

    Add files via upload

---
+++ src/Gallery.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Gallery = () =>
+(<div><h1>Галерея</h1><p>Рисунки</p><img src="http://competitions.archplatforma.ru/imgs/users/rabots/22653/biggest/file4287929112.jpg" width="545" height="662" alt=""/></div>
+);
+
+export default Gallery;
---
+++ src/Map.jsx
@@ -0,0 +1,8 @@
+import React from 'react'; 
+const Map = () => ( 
+<div> 
+<h1>Карта</h1> 
+ <iframe src="https://yandex.ua/map-widget/v1/?um=constructor%3A6259abeac2e0e20613e5986677af36c1925adf2cf1cbd30539a93ec975c664b1&amp;source=constructor" width="587" height="316" frameborder="0"></iframe> 
+</div> 
+); 
+export default Map;
